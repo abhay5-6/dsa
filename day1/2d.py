ae87b4e6c6a71705a2b7571050b75ade08f4112a
abhay5-6 <abhaytewatia84@gmail.com>
2026-08-15 23:46:07 +0530

    feat: complete floor and ceil algorithm in 2d.py and update info

--- day1/2d.py
+++ day1/2d.py
@@ -1,3 +1,23 @@
 class Solution:
-    def getFloorAndCeil(self, nums, x):
-       
+    def getFloorAndCeil(self, nums, target):
+        left = 0
+        right = len(nums) - 1
+
+        floor = -1
+        ceil = -1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] == target:
+                return [nums[mid], nums[mid]]
+
+            elif nums[mid] < target:
+                floor = nums[mid]
+                left = mid + 1
+
+            else:
+                ceil = nums[mid]
+                right = mid - 1
+
+        return [floor, ceil]
